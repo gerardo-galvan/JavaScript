08c2687985d644735fe9fc5b4d30bf03c485e9fa
gerardo-galvan <gerardo.galvan@live.com>
2022-01-01 13:51:54 -0500

    init

---
+++ AgreeOrDisagree.js
@@ -0,0 +1,18 @@
+
+// Write your function here:
+const agreeOrDisagree = (text1, text2) => {
+    if (text1 === text2) {
+      return 'You agree!'
+    } else {
+      return 'You disagree!';
+    }
+    }
+    
+    
+    
+    
+    // Uncomment the line below when you're ready to try out your function
+     console.log(agreeOrDisagree("yep", "yep")) 
+    // Should print 'You agree!'
+    // We encourage you to add more function call of your own to test your code!
+    
---
+++ CanIVote.js
@@ -0,0 +1,16 @@
+
+// Write your function here:
+const canIVote = age => {
+    if (age >= 18) {
+       // const yes = console.log('Yes, you can vote.')
+      return true;
+    }else{
+      // const no = console.log('No, you can not vote.')
+      return false;
+    }
+    }
+     
+    // Uncomment the line below when you're ready to try out your function
+      console.log(canIVote(17)) // Should print true
+    // We encourage you to add more function calls of your own to test your code!
+    
