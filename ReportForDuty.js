7fdd874cf8b3cfe93906f5bcde88f3b17045fdc4
gerardo-galvan <gerardo.galvan@live.com>
2022-01-01 18:17:39 -0500

    init

---
+++ ReportForDuty.js
@@ -0,0 +1,8 @@
+const reportingForDuty = (rank, lastName) =>{
+    let msg = rank + ' ' + lastName + ' reporting for duty!';
+    return msg;
+    }
+    
+    
+    // Uncomment the line below when you're ready to try out your function
+    console.log(reportingForDuty('Private', 'Fido')) // Should return 'Private Fido reporting for duty!'
