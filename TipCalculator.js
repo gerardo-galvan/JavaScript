fb8c245ea89bf5e95c71f55ec40038575022928d
gerardo-galvan <gerardo.galvan@live.com>
2022-01-02 16:21:29 -0500

    init

---
+++ TipCalculator.js
@@ -0,0 +1,22 @@
+const tipCalculator = (quality, total) => {
+    switch (quality) {
+  
+          case 'bad':
+              return total * .05;
+          case 'ok':
+              return total * .15;
+          case 'good':
+              return total * .20;
+          case 'excellent':
+              return total * .30;
+          default:
+              return total * .18;
+  }
+  }
+  
+   
+  
+  
+  // Uncomment the line below when you're ready to try out your function
+    console.log(tipCalculator('good', 100)) //should return 20
+  
