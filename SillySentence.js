fada4c1f897d9dd5f6afce04a079b4dadaaa5cf8
gerardo-galvan <gerardo.galvan@live.com>
2022-01-02 09:37:50 -0500

    init

---
+++ SillySentence.js
@@ -0,0 +1,13 @@
+const sillySentence = (adj, verb, noun) =>{
+    let sillySentence = `I am so ${adj} because I ${verb} coding! Time to write some more awesome ${noun}!`;
+    return sillySentence; 
+    }
+    
+    
+    
+    
+    
+    // Uncomment the line below when you're ready to try out your function
+    console.log(sillySentence('excited', 'love', 'functions')) 
+    
+ 
