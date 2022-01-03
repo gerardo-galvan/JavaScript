b8964ed3e3e8ba426c0eec781ad364992e6e52ce
gerardo-galvan <gerardo.galvan@live.com>
2022-01-03 12:01:58 -0500

    init

--- app/NumberGuesser/script.js
+++ app/NumberGuesser/script.js
@@ -2,5 +2,26 @@ let humanScore = 0;
 let computerScore = 0;
 let currentRoundNumber = 1;
 
-// Write your code below:
+const generateTarget = () => {
+    return Math.floor(Math.random() * 10);
+}
 
+const compareGuesses = (humanGuess, computerGuesses, target) => {
+if (Math.abs(target - humanGuess) < Math.abs(target - computerGuesses)){ 
+return true;
+  }else if (Math.abs(target - humanGuess) > Math.abs(target - computerGuesses)){
+return false;
+  }else {
+return true;
+  }
+}
+
+const updateScore = winner => {
+    if (winner === 'human') {
+        humanScore++;
+    } else if (winner === 'computer') {
+        computerScore++;
+    }
+}
+
+const advanceRound = () => currentRoundNumber++;
