8a86f6fff9fc302000815adce1e7f276bc33c2f9
gerardo-galvan <gerardo.galvan@live.com>
2022-01-06 12:42:22 -0500

    init

--- concepts/loops/count.js
+++ concepts/loops/count.js
@@ -1,5 +1,5 @@
-for (let count = 5; count <= 10; count++) {
+// program that loops from 5 to 10 and logs each number to the console.
+  
+  for (let count = 5; count <= 10; count++) {
     console.log(count);
   }
-
-  // program that loops from 5 to 10 and logs each number to the console.
