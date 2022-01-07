51aad01156e571703b2525f63ea4dd498231c221
gerardo-galvan <gerardo.galvan@live.com>
2022-01-06 19:38:44 -0500

    init

---
+++ concepts/loops/do_while_loop.js
@@ -0,0 +1,8 @@
+let cupsOfSugarNeeded = 2;
+let cupsAdded = 0;
+
+do {
+  cupsAdded++;
+} while (cupsAdded < cupsOfSugarNeeded)
+
+console.log(cupsAdded);
---
+++ concepts/loops/while_loop.js
@@ -0,0 +1,11 @@
+// A for loop that prints 1, 2, and 3
+for (let counterOne = 1; counterOne < 4; counterOne++){
+    console.log(counterOne);
+  }
+   
+  // A while loop that prints 1, 2, and 3
+  let counterTwo = 1;
+  while (counterTwo < 4) {
+    console.log(counterTwo);
+    counterTwo++;
+  }
