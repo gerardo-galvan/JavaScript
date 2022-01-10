39a1feeb0b59a02a20b8721b7a601c4a9f1c9fdd
gerardo-galvan <gerardo.galvan@live.com>
2022-01-10 17:47:34 -0500

    init

--- concepts/functions/factory_functions.js
+++ concepts/functions/factory_functions.js
@@ -1,12 +1,37 @@
-const monsterFactory = (name, age, energySource, catchPhrase) => {
-    return { 
-      name: name,
-      age: age, 
-      energySource: energySource,
-      scare() {
-        console.log(catchPhrase);
-      } 
-    }
-  };
+// Create a factory function named robotFactory that has two parameters model and mobile. 
+const robotFactory = (model, mobile) => {
+  // Make the function return an object. 
+  return {
+    /* In the object, add a key of model with the value of the model parameter. 
+       Add another property that has a key of mobile with a value of the mobile parameter. */
 
+    model: model,
+    mobile: mobile,
+
+    /* Property Value Shorthand
+    model,
+    mobile,
+    */
+
+    // Add a method named beep without a parameter that will log 'Beep Boop' to the console.
+    beep() {
+      console.log('Beep Boop');
+    }
+  }
+  }
   
+  // Assign to tinCan the value of calling robotFactory with the first argument of 'P-500' and the second argument of true.
+  const tinCan = robotFactory('P-500', true);
+  
+  // Call .beep() on tinCan.
+  tinCan.beep();
+
+
+// Use destructured assignment to create a const variable named functionality that extracts the functionality property of robot.
+// const { propertyName } = obj;   
+const { functionality } = robot;
+
+console.log(functionality);
+
+//  Since functionality is referencing robot.functionality we can call the methods available to robot.functionality simply through functionality.
+functionality.beep();
---
+++ concepts/objects/Destructured.Assigment.js
@@ -0,0 +1,18 @@
+
+const vampire = {
+    name: 'Dracula',
+    residence: 'Transylvania',
+    preferences: {
+      day: 'stay inside',
+      night: 'satisfy appetite'
+    }
+  };
+
+  // extract key-value pairs from objects and save them as variables.
+  // const { propertyName } = obj;
+  const { residence } = vampire; 
+
+
+  console.log(residence); // Prints 'Transylvania'
+ 
+ 
